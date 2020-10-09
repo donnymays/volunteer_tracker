6bc793330eac76e21256acf73a2a6e0bbdc5103c
donnymays <dtmays2@gmail.com>
2020-10-09 08:41:41 -0700

    successful fail of project.all spec

--- lib/project.rb
+++ lib/project.rb
@@ -15,4 +15,15 @@ class Project
   def ==(param)
     self.title() == param.title()
   end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
 end
