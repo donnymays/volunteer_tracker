38c2837771206f9dceba13f3f7b94452e1aa5c98
donnymays <dtmays2@gmail.com>
2020-10-09 09:00:28 -0700

    successful fail of project.update spec

--- lib/project.rb
+++ lib/project.rb
@@ -34,8 +34,8 @@ class Project
     Project.new({:title => title, :id => id})
   end
 
-  def update(title)
-    @title = title
-    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{id};")
+  def update(attributes)
+    @title = attributes.fetch(:title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
 end
