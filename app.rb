a30435de9b7035fa87e21f5c0a18cf709feb66b0
donnymays <dtmays2@gmail.com>
2020-10-09 10:01:24 -0700

    passing project creation path integration spec

--- app.rb
+++ app.rb
@@ -7,3 +7,19 @@ also_reload('lib/**/*.rb')
 require("pg")
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do
+  title = params[:title]
+  project = Project.new({:title => title, :id => nil})
+  project.save()
+  redirect to('/projects')
+end
