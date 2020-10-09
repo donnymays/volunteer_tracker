8df43cb8a498f64afb64e2dd927f6e760cf822ae
donnymays <dtmays2@gmail.com>
2020-10-09 09:19:15 -0700

    successful fail of volunteer.== spec

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -7,4 +7,13 @@ class Volunteer
     @id = attributes.fetch(:id)
     @proj_id = attributes.fetch(:proj_id)
   end
+
+  def ==(attr)
+    if attr != nil
+      (self.name() == attr.name() && (self.proj_id() == attr.proj_id()))
+    else
+      false
+    end
+  end
+
 end
