b1c180465ecfdd1afeab6f4de67253b9aeecce9f
donnymays <dtmays2@gmail.com>
2020-10-09 09:19:43 -0700

    successful pass of volunteer.== spec

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -17,7 +17,7 @@ describe Volunteer do
   describe '#==' do
     it 'checks for equality based on the name of a volunteer' do
       volunteer1 = Volunteer.new({:name => 'Jane', :proj_id => 1, :id => nil})
-      volunteer2 = Volunteer.new({:name => 'ane', :proj_id => 1, :id => nil})
+      volunteer2 = Volunteer.new({:name => 'Jane', :proj_id => 1, :id => nil})
       expect(volunteer1 == volunteer2).to eq true
     end
   end
